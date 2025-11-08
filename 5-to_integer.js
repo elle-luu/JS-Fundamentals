157d0eb23815e32c373c51a140b3676b385fb9dc
Elle-Shavor Senoya Luu <senoyaluu1@gmail.com>
2025-11-08 20:47:01 +0000

    Add 4-concat.js script

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,8 @@
 const arg = process.argv[2];
 const num = parseInt(arg, 10);
 
-if (isNaN(num)) {
+if (arg === undefined || isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${num}`
+  console.log(`My number: ${num}`);
+}
